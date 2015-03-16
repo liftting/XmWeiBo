27702c7525a56bcfcb06f8c053d5d08daad1e9ab
wangming <wangming@rd.tuan800.com>
2015-03-16 18:57:14 +0800

    update main layout

--- XmWei/app/src/main/java/wm/xmwei/ui/activity/XmMainAct.java
+++ XmWei/app/src/main/java/wm/xmwei/ui/activity/XmMainAct.java
@@ -25,6 +25,8 @@ public class XmMainAct extends BaseActivity {
     private DrawerArrowDrawable drawerArrow;
     private boolean drawerArrowColor;
 
+    private View mDrawerMenu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +36,8 @@ public class XmMainAct extends BaseActivity {
         ab.setHomeButtonEnabled(true);
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        mDrawerMenu = findViewById(R.id.navigation_drawer);
 
 
         drawerArrow = new DrawerArrowDrawable(this) {
@@ -59,84 +63,17 @@ public class XmMainAct extends BaseActivity {
         mDrawerLayout.setDrawerListener(mDrawerToggle);
         mDrawerToggle.syncState();
 
-
-        String[] values = new String[]{
-            "Stop Animation (Back icon)",
-            "Stop Animation (Home icon)",
-            "Start Animation",
-            "Change Color",
-            "GitHub Page",
-            "Share",
-            "Rate"
-        };
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-            android.R.layout.simple_list_item_1, android.R.id.text1, values);
-//        mDrawerList.setAdapter(adapter);
-//        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @SuppressLint("ResourceAsColor")
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view,
-//                                    int position, long id) {
-//                switch (position) {
-//                    case 0:
-//                        mDrawerToggle.setAnimateEnabled(false);
-//                        drawerArrow.setProgress(1f);
-//                        break;
-//                    case 1:
-//                        mDrawerToggle.setAnimateEnabled(false);
-//                        drawerArrow.setProgress(0f);
-//                        break;
-//                    case 2:
-//                        mDrawerToggle.setAnimateEnabled(true);
-//                        mDrawerToggle.syncState();
-//                        break;
-//                    case 3:
-//                        if (drawerArrowColor) {
-//                            drawerArrowColor = false;
-//                            drawerArrow.setColor(R.color.ldrawer_color);
-//                        } else {
-//                            drawerArrowColor = true;
-//                            drawerArrow.setColor(R.color.drawer_arrow_second_color);
-//                        }
-//                        mDrawerToggle.syncState();
-//                        break;
-//                    case 4:
-//                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/IkiMuhendis/LDrawer"));
-//                        startActivity(browserIntent);
-//                        break;
-//                    case 5:
-//                        Intent share = new Intent(Intent.ACTION_SEND);
-//                        share.setType("text/plain");
-//                        share.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//                        share.putExtra(Intent.EXTRA_SUBJECT,
-//                            getString(R.string.app_name));
-//                        share.putExtra(Intent.EXTRA_TEXT, getString(R.string.app_description) + "\n" +
-//                            "GitHub Page :  https://github.com/IkiMuhendis/LDrawer\n" +
-//                            "Sample App : https://play.google.com/store/apps/details?id=" +
-//                            getPackageName());
-//                        startActivity(Intent.createChooser(share,
-//                            getString(R.string.app_name)));
-//                        break;
-//                    case 6:
-//                        String appUrl = "https://play.google.com/store/apps/details?id=" + getPackageName();
-//                        Intent rateIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(appUrl));
-//                        startActivity(rateIntent);
-//                        break;
-//                }
-//
-//            }
-//        });
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-//        if (item.getItemId() == android.R.id.home) {
-//            if (mDrawerLayout.isDrawerOpen(mDrawerList)) {
-//                mDrawerLayout.closeDrawer(mDrawerList);
-//            } else {
-//                mDrawerLayout.openDrawer(mDrawerList);
-//            }
-//        }
+        if (item.getItemId() == android.R.id.home) {
+            if (mDrawerLayout.isDrawerOpen(mDrawerMenu)) {
+                mDrawerLayout.closeDrawer(mDrawerMenu);
+            } else {
+                mDrawerLayout.openDrawer(mDrawerMenu);
+            }
+        }
         return super.onOptionsItemSelected(item);
     }
 
