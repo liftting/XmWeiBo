4000a2431d1f531df661db361e5f75f4f8381e39
wangming <wangming@rd.tuan800.com>
2015-03-17 21:19:41 +0800

    add data

--- XmWei/app/src/main/java/wm/xmwei/ui/fragment/base/XmBaseListFragment.java
+++ XmWei/app/src/main/java/wm/xmwei/ui/fragment/base/XmBaseListFragment.java
@@ -1,7 +1,127 @@
 package wm.xmwei.ui.fragment.base;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import wm.xmwei.R;
+import wm.xmwei.bean.DataListDomain;
+import wm.xmwei.ui.view.pulltorefresh.PullToRefreshBase;
+import wm.xmwei.ui.view.pulltorefresh.PullToRefreshListView;
+import wm.xmwei.ui.view.pulltorefresh.extras.SoundPullEventListener;
+
 /**
- * Created by wm on 15-3-17.
+ *
  */
-public class XmBaseListFragment extends XmBaseFragment {
+public abstract class XmBaseListFragment<T extends DataListDomain> extends XmBaseFragment {
+
+    private PullToRefreshListView mPullToRefreshListView;
+    private ProgressBar mProgressBar;
+    protected View mFooterView;
+    protected BaseAdapter mBaseDataAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        mPullToRefreshListView.setOnRefreshListener(listViewOnRefreshListener);
+        mPullToRefreshListView.setOnLastItemVisibleListener(listViewOnLastItemVisibleListener);
+        mPullToRefreshListView.setOnScrollListener(listViewOnScrollListener);
+        mPullToRefreshListView.setOnItemClickListener(listViewOnItemClickListener);
+        mPullToRefreshListView.setOnPullEventListener(getPullEventListener());
+
+        createDataListAdapter();
+
+    }
+
+    private PullToRefreshBase.OnLastItemVisibleListener listViewOnLastItemVisibleListener
+            = new PullToRefreshBase.OnLastItemVisibleListener() {
+        @Override
+        public void onLastItemVisible() {
+
+        }
+    };
+
+    private PullToRefreshBase.OnRefreshListener<ListView> listViewOnRefreshListener
+            = new PullToRefreshBase.OnRefreshListener<ListView>() {
+        @Override
+        public void onRefresh(PullToRefreshBase<ListView> refreshView) {
+
+        }
+    };
+
+    private SoundPullEventListener<ListView> getPullEventListener() {
+        SoundPullEventListener<ListView> listener = new SoundPullEventListener<ListView>(
+                getActivity());
+        return listener;
+    }
+
+    private AdapterView.OnItemClickListener listViewOnItemClickListener
+            = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        }
+
+    };
+
+    private AbsListView.OnScrollListener listViewOnScrollListener
+            = new AbsListView.OnScrollListener() {
+        @Override
+        public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+        }
+
+        @Override
+        public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
+                             int totalItemCount) {
+        }
+    };
+
+    public PullToRefreshListView getPullToRefreshListView() {
+        return mPullToRefreshListView;
+    }
+
+    public ListView getListView() {
+        return mPullToRefreshListView.getRefreshableView();
+    }
+
+    protected void refreshLayout(T bean) {
+        if (bean != null && bean.getSize() > 0) {
+            mProgressBar.setVisibility(View.INVISIBLE);
+        } else if (bean == null || bean.getSize() == 0) {
+            mProgressBar.setVisibility(View.INVISIBLE);
+        } else if (bean.getSize() == bean.getTotal_number()) {
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    protected abstract void createDataListAdapter();
+
+    public abstract T getDataList();
+
+    protected abstract void onItemClick(AdapterView parent, View view, int position, long id);
+
+
 }
