ec178ca087f641a3acaa27458ad859422b31617c
wangming <wangming@rd.tuan800.com>
2015-03-19 17:22:31 +0800

    add image loader

---
+++ XmWei/app/src/main/java/wm/xmwei/core/image/universalimageloader/XmImageLoader.java
@@ -0,0 +1,72 @@
+package wm.xmwei.core.image.universalimageloader;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.widget.ImageView;
+
+import wm.xmwei.R;
+import wm.xmwei.XmApplication;
+import wm.xmwei.core.image.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import wm.xmwei.core.image.universalimageloader.core.DisplayImageOptions;
+import wm.xmwei.core.image.universalimageloader.core.ImageLoader;
+import wm.xmwei.core.image.universalimageloader.core.ImageLoaderConfiguration;
+import wm.xmwei.core.image.universalimageloader.core.assist.QueueProcessingType;
+import wm.xmwei.core.image.universalimageloader.core.display.FadeInBitmapDisplayer;
+
+/**
+ *
+ *
+ */
+public class XmImageLoader {
+
+    private static XmImageLoader inst;
+
+    public static XmImageLoader getInstance() {
+        if (inst == null) {
+            inst = new XmImageLoader();
+            inst.initImageLoader(XmApplication.getInstance(), R.color.floralwhite, R.color.floralwhite, R.color.floralwhite);
+        }
+        return inst;
+    }
+
+    ImageLoader imageLoader;
+    DisplayImageOptions options;
+
+    private static int FADE_IN_DURATION = 50;// 淡入效果的持续时间
+
+    /**
+     * 初始化图片加载类配置信息*
+     */
+    public void initImageLoader(Context context, int mg_default_list, int img_default_listUri, int nomal_mg_default_list) {
+        ImageLoaderConfiguration.Builder bin = new ImageLoaderConfiguration.Builder(context)
+                .threadPriority(Thread.MIN_PRIORITY)//加载图片的线程数
+                .denyCacheImageMultipleSizesInMemory() //解码图像的大尺寸将在内存中缓存先前解码图像的小尺寸。
+                .discCacheFileNameGenerator(new Md5FileNameGenerator())//设置磁盘缓存文件名称
+                .tasksProcessingOrder(QueueProcessingType.LIFO)//设置加载显示图片队列进程  后进先出
+                .discCacheFileCount(500);
+
+//        if (!AppConfig.LOG_CLOSED&&DevRunningTime.isShowImageErr)bin.enableLogging();
+
+        ImageLoaderConfiguration config = bin.build();
+        imageLoader = ImageLoader.getInstance();
+        imageLoader.init(config);
+
+        DisplayImageOptions.Builder disBin = new DisplayImageOptions.Builder()
+                .showStubImage(mg_default_list)
+                .showImageForEmptyUri(img_default_listUri)
+                .cacheInMemory(true)// .cacheInMemory()
+                .cacheOnDisc(true)
+                .displayer(new FadeInBitmapDisplayer(FADE_IN_DURATION))// 添加淡入效果
+                .bitmapConfig(Bitmap.Config.ARGB_8888);
+
+        disBin.showImageOnFail(nomal_mg_default_list);
+        options = disBin.build();
+    }
+
+    public void loadImage(String msgImgUrl, ImageView image) {
+        if (msgImgUrl == null) return;
+        if (msgImgUrl == image.getTag()) return;
+        imageLoader.displayImage(msgImgUrl, image, options);
+    }
+
+}
---
+++ XmWei/app/src/main/java/wm/xmwei/ui/activity/login/XmUserBingActivity.java
@@ -0,0 +1,194 @@
+package wm.xmwei.ui.activity.login;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import wm.xmwei.R;
+import wm.xmwei.bean.UserBingDomain;
+import wm.xmwei.bean.UserDomain;
+import wm.xmwei.core.database.datatask.DbUserBingTask;
+import wm.xmwei.core.image.universalimageloader.XmImageLoader;
+import wm.xmwei.core.image.universalimageloader.core.ImageLoader;
+import wm.xmwei.ui.activity.BaseActivity;
+
+/**
+ * this is show user bing info activity
+ */
+public class XmUserBingActivity extends BaseActivity implements View.OnClickListener,
+        LoaderManager.LoaderCallbacks<List<UserBingDomain>> {
+
+    private Button mBtnUserBing;
+    private List<UserBingDomain> mBingDataList;
+
+    private ListView mBingList;
+    private UserBingAdapter mBingDataAdapter;
+
+    private final int LOADER_ID = 0;
+    private final int ADD_ACCOUNT_REQUEST_CODE = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layer_user_bing_main);
+
+        mBtnUserBing = (Button) findViewById(R.id.btn_bing_user);
+        mBtnUserBing.setOnClickListener(this);
+        mBingList = (ListView) findViewById(R.id.v_user_bing_list);
+
+        mBingDataList = new ArrayList<UserBingDomain>();
+        mBingDataAdapter = new UserBingAdapter();
+        mBingList.setAdapter(mBingDataAdapter);
+
+        this.getSupportLoaderManager().initLoader(LOADER_ID, null, this);
+
+        refresh();
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(XmUserBingActivity.this, XmSoLoginActivity.class);
+        startActivityForResult(intent, ADD_ACCOUNT_REQUEST_CODE);
+    }
+
+    @Override
+    public Loader<List<UserBingDomain>> onCreateLoader(int id, Bundle args) {
+
+        return new UserBingDataLoader(XmUserBingActivity.this);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<UserBingDomain>> loader, List<UserBingDomain> data) {
+        mBingDataList = data;
+        mBingDataAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<UserBingDomain>> loader) {
+        mBingDataList = new ArrayList<UserBingDomain>();
+        mBingDataAdapter.notifyDataSetChanged();
+    }
+
+    //数据load
+    private static class UserBingDataLoader extends AsyncTaskLoader<List<UserBingDomain>> {
+
+        public UserBingDataLoader(Context context) {
+            super(context);
+        }
+
+        @Override
+        public List<UserBingDomain> loadInBackground() {
+            return DbUserBingTask.getUserBingList();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == ADD_ACCOUNT_REQUEST_CODE && resultCode == RESULT_OK) {
+            refresh();
+        }
+    }
+
+    private void refresh() {
+        getSupportLoaderManager().getLoader(LOADER_ID).forceLoad();
+    }
+
+
+    private class UserBingAdapter extends BaseAdapter {
+        private int defaultBG;
+
+        public UserBingAdapter() {
+            defaultBG = getResources().getColor(R.color.transparent);
+        }
+
+        @Override
+        public int getCount() {
+            return mBingDataList.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return mBingDataList.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return Long.valueOf(mBingDataList.get(i).getUid());
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+        @Override
+        public View getView(final int i, View view, ViewGroup viewGroup) {
+            ViewHolder holder;
+            if (view == null || view.getTag() == null) {
+                LayoutInflater layoutInflater = getLayoutInflater();
+                View mView = layoutInflater
+                        .inflate(R.layout.layer_user_bing_list_item, viewGroup, false);
+                holder = new ViewHolder();
+                holder.root = mView.findViewById(R.id.listview_root);
+                holder.name = (TextView) mView.findViewById(R.id.account_name);
+                holder.avatar = (ImageView) mView.findViewById(R.id.imageView_avatar);
+                holder.tokenInvalid = (TextView) mView.findViewById(R.id.token_expired);
+                view = mView;
+            } else {
+                holder = (ViewHolder) view.getTag();
+            }
+
+            holder.root.setBackgroundColor(defaultBG);
+
+            if (mBingDataList.get(i).getInfo() != null) {
+                holder.name.setText(mBingDataList.get(i).getInfo().getScreen_name());
+            } else {
+                holder.name.setText(mBingDataList.get(i).getUsernick());
+            }
+
+            if (!TextUtils.isEmpty(mBingDataList.get(i).getAvatar_url())) {
+                downloadAvatar(holder.avatar, mBingDataList.get(i).getInfo());
+            }
+
+//            holder.tokenInvalid.setVisibility(!Utility.isTokenValid(accountList.get(i)) ? View.VISIBLE : View.GONE);
+            return view;
+        }
+    }
+
+    class ViewHolder {
+        View root;
+        TextView name;
+        ImageView avatar;
+        TextView tokenInvalid;
+    }
+
+    private void downloadAvatar(ImageView view, UserDomain userDomain) {
+        if (userDomain == null) {
+            view.setImageResource(R.color.floralwhite);
+            return;
+        }
+
+        String url = userDomain.getAvatar_large();
+        XmImageLoader.getInstance().loadImage(url, view);
+    }
+
+
+}
