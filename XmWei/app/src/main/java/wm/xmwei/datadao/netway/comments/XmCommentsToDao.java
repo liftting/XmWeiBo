68a20ab640b148c443931db239327a884b8d6aad
wangming <wangming@rd.tuan800.com>
2015-03-20 18:40:43 +0800

    update data request

--- XmWei/app/src/main/java/wm/xmwei/datadao/netway/comments/XmCommentsToDao.java
+++ XmWei/app/src/main/java/wm/xmwei/datadao/netway/comments/XmCommentsToDao.java
@@ -25,12 +25,12 @@ public class XmCommentsToDao implements ICommentsDao {
 
         Map<String, String> map = new HashMap<String, String>();
         map.put("access_token", access_token);
-        map.put("since_id", "0");
-        map.put("max_id", "100");
+        map.put("since_id", since_id);
+        map.put("max_id", max_id);
         map.put("count", count);
         map.put("page", page);
         map.put("filter_by_author", filter_by_author);
-        map.put("filter_by_source", filter_by_source);
+//        map.put("filter_by_source", filter_by_source);
 
         String jsonData = NetWorker.getInstance().executeNormalTask(HttpMethod.Get, url, map);
 
@@ -81,18 +81,28 @@ public class XmCommentsToDao implements ICommentsDao {
         this.filter_by_author = filter_by_author;
     }
 
+/**
+ * 根据微博ID返回某条微博的评论列表
+ * @param id 需要查询的微博ID。
+ * @param since_id 若指定此参数，则返回ID比since_id大的评论（即比since_id时间晚的评论），默认为0。
+ * @param max_id 若指定此参数，则返回ID小于或等于max_id的评论，默认为0。
+ * @param count 单页返回的记录条数，默认为50
+ * @param page 返回结果的页码，默认为1。
+ * @param filter_by_author 作者筛选类型，0：全部、1：我关注的人、2：陌生人，默认为0。
+ * @param listener
+ */
+
     protected String access_token;
-    private String since_id;
-    private String max_id;
-    private String count;
-    private String page;
-    private String filter_by_author = "";
+    private String since_id = "0";
+    private String max_id = "0";
+    private String count = "50";
+    private String page = "1";
+    private String filter_by_author = "0";
     private String filter_by_source = "";
 
     public XmCommentsToDao(String access_token) {
         this.access_token = access_token;
         this.count = "20";
-        this.page = "1";
     }
 
     protected String getUrl() {
